544a628b20b18d07bc54adfcfbae8f022081cc7a
DeadBoy <tokrar322@mail.ru>
2021-11-13 17:33:30 +0300

    modded laboratory work 3

---
+++ src/laboratorywork3/MyQueue2.kt
@@ -0,0 +1,55 @@
+package laboratorywork3
+
+class MyQueue2(
+    var pop: Int = -1,
+    private var array: Array<String?> = arrayOfNulls(10)
+) {
+
+    fun insert(word: String?) {
+        if (array.size - 1 == pop) {
+            val arrayHelper = array
+            array = arrayOfNulls<String>((pop * 1.5).toInt())
+            for (i in arrayHelper.indices) {
+                array[i] = arrayHelper[i]
+            }
+        }
+        array[++pop] = word
+    }
+
+    fun removeFirst() {
+        val firstEl = array[0]
+        val arrayHelper = array
+        array = arrayOfNulls<String>(pop-1)
+        for (i in 1 until arrayHelper.size) {
+            array[i-1] = arrayHelper[i]
+        }
+    }
+
+    fun remove() = array[pop].also {
+        array[pop--] = ""
+    }
+
+    fun isEmpty() = pop == -1
+
+    fun headValue() = array[pop]
+
+    fun rearValue() = array[0]
+
+    fun clear() {
+        array = arrayOfNulls(10)
+    }
+
+    fun size() = pop + 1
+
+    fun print(): String {
+        var string = ""
+        for (i in array.indices) {
+            if (array[i] != null) {
+                string += array[i] + " "
+            }
+        }
+        return string
+    }
+
+
+}
---
+++ src/laboratorywork3/Runner2.kt
@@ -0,0 +1,58 @@
+package laboratorywork3
+
+import java.io.BufferedReader
+import java.io.File
+
+fun main() {
+    var bufferedReader: BufferedReader = File("laboratory3_1.txt").bufferedReader()
+    var inputString = bufferedReader.use { it.readText()} // чтение данных из файла
+    val families1 = inputString.split("\r\n") // деление текста на фамилии
+
+    bufferedReader = File("laboratory3_2.txt").bufferedReader()
+    inputString = bufferedReader.use { it.readText() } // чтение данных из файла 2
+    val families2 = inputString.split("\r\n") // деление текста на фамилии
+
+    val queue1 = MyQueue2() // создание первой очереди
+    val queue2 = MyQueue2() // создание второй очереди
+    families1.map { queue1.insert(it) } // добавление элементов в первую очередь
+    families2.map { queue2.insert(it) } // добавление элементов во второую очередь
+
+    println("Фамилии из первого файла: ${queue1.print()}") // вывод элементов из первой очереди
+    println("Фамилии из второго файла: ${queue2.print()}") // вывод элементов из второй очереди
+
+    val unionQueue = unionQueues(queue1, queue2) // создание объединенной очереди
+    println("Объединенные фамилии: ${unionQueue.print()}") // вывод объединенной очереди
+
+    File("laboratory3_result.txt").printWriter().use { out -> // запись фамилий в файл
+        for (i in 0 until unionQueue.size()) {
+            out.println(unionQueue.remove())
+        }
+    }
+}
+
+fun unionQueues(queue1: MyQueue2, queue2: MyQueue2): MyQueue2 { // метод для объединения очередей
+    val unionQueue = MyQueue2() // создаём объединённую очередь
+    val length: Int = if (queue1.size() < queue2.size()) { // ищём минимальную очередь для итераций
+        queue1.size()
+    } else {
+        queue2.size()
+    }
+
+
+    for (i in 0 until length) { // итерируемся по очередям и добавляем поочередно элементы в объединенную очередь
+        unionQueue.insert(queue1.remove())
+        unionQueue.insert(queue2.remove())
+    }
+
+    if (queue1.size() < queue2.size()) { // добавляем оставшиеся элементы из очереди, которая длиннее
+        for (i in 0 until  queue2.size()) {
+            unionQueue.insert(queue2.remove())
+        }
+    } else {
+        for (i in 0 until queue1.size()) {
+            unionQueue.insert(queue1.remove())
+        }
+    }
+
+    return unionQueue
+}
